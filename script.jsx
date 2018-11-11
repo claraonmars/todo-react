c7992ad44948db9489c922d02f3042653786bd9e
claraonmars <claraonmars@hotmail.com>
2018-11-11 13:33:06 +0800

    Add length validation and error message

--- script.jsx
+++ script.jsx
@@ -8,17 +8,31 @@ class List extends React.Component {
 
   state = {
     list : [],
-    word : ""
+    word : "",
+    class: 'normal',
+    error: ''
   }
 
   changeHandler(event){
     this.setState({word:event.target.value});
     console.log("change", event.target.value);
+
+    // if input is longer than 20 => change className to warning
+    if( event.target.value.length > 20){
+        let warning = this.state.currentClass
+        warning = 'warning'
+        this.setState({class: warning, error: 'Error: Input too long'})
+    }
+    else{
+        this.setState({class:'normal'})
+    }
+
   }
 
   addItem(){
     let listarr = [...this.state.list];
     listarr.push(this.state.word);
+
     // push todoitem and clear out input value
     this.setState({list: listarr, word: ''});
   }
@@ -34,7 +48,8 @@ class List extends React.Component {
             <li key={i}>{item}</li>
           ))}
         </ul>
-          <input onChange={this.changeHandler} value={this.state.word}/>
+            <p>{this.state.error}</p>
+          <input className={this.state.class} onChange={this.changeHandler} value={this.state.word}/>
           <button onClick={this.addItem}>add item</button>
         </div>
       );
